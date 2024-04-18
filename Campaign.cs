9f5a4a036230d5b58d6ef3de4f307cf3f19c5995
Dana Larose <ywg.dana@gmail.com>
2024-04-18 15:36:29 -0500

    Kobold foremen may be too tough for level 2 of the dungeon

--- Campaign.cs
+++ Campaign.cs
@@ -405,7 +405,7 @@ class PreGameHandler(UserInterface ui)
     else
     {
       int seed = DateTime.Now.GetHashCode();
-      seed = -998002449;
+      seed = -1522110263;
       //seed = 176296748;
       // -758465673 this seed doesn't have a valid entrance loc?
 
