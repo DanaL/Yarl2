e96480d4494f0cd27f4c5af8bd17904ffcf6dac8
Dana Larose <ywg.dana@gmail.com>
2024-05-09 11:33:23 -0500

    Look in user's homedir first for options file

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 
 using Yarl2;
 
-var options = Options.LoadOptions("options.json");
+var options = Options.LoadOptions();
 
 UserInterface display;
 if (options.Display == "Bearlib")
-    display = new BLUserInferface("Dana's Delve 0.0.1 + Bearlib", options);
+  display = new BLUserInferface("Dana's Delve 0.0.1 + Bearlib", options);
 else
-    display = new SDLUserInterface("Dana's Delve 0.0.1 + SDL", options);
+  display = new SDLUserInterface("Dana's Delve 0.0.1 + SDL", options);
 
 display.TitleScreen();
 
@@ -16,42 +16,60 @@ var pgh = new PreGameHandler(display);
 var gameState = pgh.StartUp(options);
 if (gameState is not null)
 {
-    display.GameLoop(gameState);
+  display.GameLoop(gameState);
 }
 
 namespace Yarl2
 {
-    public class Options
+  public class Options
+  {
+    public string? Display { get; set; }
+    public int FontSize { get; set; }
+    public bool BumpToOpen { get; set; }
+
+    public static Options LoadOptions()
     {
-        public string? Display { get; set; }
-        public int FontSize { get; set; }
-        public bool BumpToOpen { get; set; }
+      // Okay, we'll check for the options file in the user's home dir, then the
+      // folder the executable lives in and failing that, use some default options
 
-        public static Options LoadOptions(string path)
+      Options options = new()
+      {
+        Display = "Bearlib",
+        FontSize = 12,
+        BumpToOpen = true
+      };
+
+      var userDir = new DirectoryInfo(Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile));
+      if (userDir.Exists)
+      {
+        string optionsPath = "ddoptions.json";
+
+        foreach (FileInfo file in userDir.GetFiles())
         {
-            Options options = new Options()
-            {
-                Display = "Bearlib",
-                FontSize = 12,
-                BumpToOpen = true
-            };
-
-            if (File.Exists(path))
-            {
-                var json = File.ReadAllText(path);
-                var opts = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
-                if (opts is not null) 
-                {
-                    if (opts.TryGetValue("Display", out var displayValue))            
-                        options.Display = displayValue;
-                    if (opts.TryGetValue("FontSize", out var fsValue))
-                        options.FontSize = int.Parse(fsValue);
-                    if (opts.TryGetValue("BumpToOpen", out var btoValue))
-                        options.BumpToOpen = bool.Parse(btoValue);
-                }
-            }
-            
-            return options;
+          if (file.Name == "ddoptions.json")
+          {
+            optionsPath = file.FullName;
+            break;
+          }
         }
+
+        if (File.Exists(optionsPath))
+        {
+          var json = File.ReadAllText(optionsPath);
+          var opts = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+          if (opts is not null)
+          {
+            if (opts.TryGetValue("Display", out var displayValue))
+              options.Display = displayValue;
+            if (opts.TryGetValue("FontSize", out var fsValue))
+              options.FontSize = int.Parse(fsValue);
+            if (opts.TryGetValue("BumpToOpen", out var btoValue))
+              options.BumpToOpen = bool.Parse(btoValue);
+          }
+        }
+      }
+
+      return options;
     }
+  }
 }
