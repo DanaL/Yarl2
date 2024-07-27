d12a1e8e980689957f270fdc58efd6c9e2d94b05
Dana Larose <ywg.dana@gmail.com>
2024-07-27 14:37:43 -0500

    Switching to building tables of item rarities, then selecting from those tables depending on which dungeon level we are generating treasure for

--- Treasure.cs
+++ Treasure.cs
@@ -11,71 +11,28 @@
 
 namespace Yarl2;
 
-record TreasureOdds(double Lo, double Hi, ItemNames Name, int Min, int Max);
-
 class Treasure
-{
-  static Dictionary<(int, int), List<TreasureOdds>> TreasureTable = [];
-
+{  
+  static readonly List<ItemNames> CommonItems = [ ItemNames.TORCH, ItemNames.TORCH, ItemNames.TORCH, ItemNames.TORCH,
+                                                  ItemNames.ZORKMIDS_PITTANCE, ItemNames.ZORKMIDS_PITTANCE, ItemNames.ZORKMIDS_PITTANCE,
+                                                  ItemNames.POTION_HEALING, ItemNames.ANTIDOTE, ItemNames.DAGGER, ];
+  static readonly List<ItemNames> UncommonItems = [ ItemNames.TORCH, ItemNames.ZORKMIDS_PITTANCE, ItemNames.ZORKMIDS_PITTANCE,
+                                                    ItemNames.ZORKMIDS_PITTANCE, ItemNames.ZORKMIDS_PITTANCE, ItemNames.POTION_HEALING,
+                                                    ItemNames.POTION_HEALING, ItemNames.ANTIDOTE, ItemNames.ANTIDOTE, ItemNames.DAGGER,
+                                                    ItemNames.SHORTSHORD, ItemNames.SHIELD, ItemNames.SCROLL_BLINK, 
+                                                    ItemNames.SCROLL_MAGIC_MAP ]; 
+    
   public static List<Item> PoorTreasure(int numOfItems, Random rng, GameObjectDB objDb)
   {
     List<Item> loot = [];
 
     for (int j = 0; j < numOfItems; j++)
     {
-      double roll = rng.NextDouble();
-      if (roll > 0.95)
-      {
-        loot.Add(ItemFactory.Get(ItemNames.POTION_HEALING, objDb));
-      }
-      else if (roll > 0.80)
-      {
-        loot.Add(ItemFactory.Get(ItemNames.TORCH, objDb));        
-      }
-      else if (roll > 0.5)
-      {
-        var cash = ItemFactory.Get(ItemNames.ZORKMIDS, objDb);
-        cash.Value = rng.Next(3, 11);        
-      }
+      var name = CommonItems[rng.Next(CommonItems.Count)];
+      loot.Add(GenerateItem(name, objDb, rng));
     }
     
     return loot;
-  }
-
-  // Eventually I need to add file format checking...
-  static void LoadTreasureTables()
-  {
-    var lines = File.ReadAllLines("data/treasure_tables.txt");
-
-    int dungeonID = -1, level = -1;
-
-    foreach (string line in lines)
-    {
-      if (line.StartsWith("Dungeon"))
-      {
-        var parts = line.Split(' ');
-        dungeonID = int.Parse(parts[1]);
-        level = int.Parse(parts[3]);
-      }
-      else
-      {
-        // 0.0,0.33,TORCH,1,1
-        var parts = line.Split(',');
-        double lo = double.Parse(parts[0]);
-        double hi = double.Parse(parts[1]);
-        Enum.TryParse(parts[2], out ItemNames name);
-        int min = int.Parse(parts[3]);
-        int max = int.Parse(parts[4]);
-
-        var key = (dungeonID, level);
-        if (!TreasureTable.ContainsKey(key))
-        {
-          TreasureTable.Add(key, []);
-        }
-
-        TreasureTable[key].Add(new TreasureOdds(lo, hi, name, min, max));
-      }
-    }
   }
 
   static void AddToLevel(Item item, GameObjectDB objDb, Map level, int dungeonID, int levelNum, Random rng)
@@ -86,51 +43,41 @@ class Treasure
     objDb.SetToLoc(loc, item);
   }
 
+  static Item GenerateItem(ItemNames name, GameObjectDB objDb, Random rng)
+  {
+    if (name == ItemNames.ZORKMIDS_PITTANCE) 
+    {
+      var zorkmids = ItemFactory.Get(ItemNames.ZORKMIDS, objDb);
+      zorkmids.Value = rng.Next(1, 11);
+      return zorkmids;
+    }
+    else
+    {
+      return ItemFactory.Get(name, objDb);
+    }
+  }
   public static void AddTreasureToDungeonLevel(GameObjectDB objDb, Map level, int dungeonID, int levelNum, Random rng)
   {
-    if (TreasureTable.Count == 0)
+    if (levelNum == 0) 
     {
-      LoadTreasureTables();
+      int numItems = rng.Next(1, 4);
+      for (int j = 0; j < numItems; j++) 
+      {
+        var name = CommonItems[rng.Next(CommonItems.Count)];
+        var item = GenerateItem(name, objDb, rng);
+        AddToLevel(item, objDb, level, dungeonID, levelNum, rng);
+      }
     }
-
-    var foo = rng.NextDouble();
-    foo = rng.NextDouble();
-    foo = rng.NextDouble();
-
-    var key = (dungeonID, levelNum);
-    if (TreasureTable.TryGetValue(key, out List<TreasureOdds>? value))
+    else if (levelNum == 1)
     {
-      var treasureTable = value;
-
       int numItems = rng.Next(1, 5);
       for (int j = 0; j < numItems; j++)
       {
         double roll = rng.NextDouble();
-        foreach (var ti in treasureTable)
-        {
-          if (roll >= ti.Lo && roll < ti.Hi)
-          {
-            int amt = ti.Max > 1 ? rng.Next(ti.Min, ti.Max + 1) : 1;
-
-            // Zorkminds are odd ducks...
-            if (ti.Name == ItemNames.ZORKMIDS)
-            {
-              var zorkmids = ItemFactory.Get(ti.Name, objDb);
-              zorkmids.Value = amt;
-              AddToLevel(zorkmids, objDb, level, dungeonID, levelNum, rng);
-            }
-            else
-            {
-              for (int k = 0; k < amt; k++)
-              {
-                var item = ItemFactory.Get(ti.Name, objDb);
-                AddToLevel(item, objDb, level, dungeonID, levelNum, rng);
-              }
-            }
-            break;
-          }
-        }
+        var name = roll <= 0.9 ? CommonItems[rng.Next(CommonItems.Count)] : UncommonItems[rng.Next(UncommonItems.Count)];
+        var item = GenerateItem(name, objDb, rng);
+        AddToLevel(item, objDb, level, dungeonID, levelNum, rng);
       }
-    }       
+    }
   }
 }
