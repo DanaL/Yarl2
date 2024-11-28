21fd6e18c2f0bf408cbbbca788f5a122a78c6866
Dana Larose <ywg.dana@gmail.com>
2024-11-28 11:10:26 -0600

    Didn't really need Campaign and Dungeon classes in their own file

--- GameObjectDB.cs
+++ GameObjectDB.cs
@@ -470,3 +470,34 @@ class GameObjectDB
     return listeners;
   }
 }
+
+// A structure to store info about a dungeon
+class Dungeon(int ID, string arrivalMessage)
+{
+  public int ID { get; init; } = ID;
+  public Dictionary<Loc, Glyph> RememberedLocs = [];
+  public Dictionary<int, Map> LevelMaps = [];
+  public string ArrivalMessage { get; } = arrivalMessage;
+
+  public void AddMap(Map map)
+  {
+    int id = LevelMaps.Count == 0 ? 0 : LevelMaps.Keys.Max() + 1;
+    LevelMaps.Add(id, map);
+  }
+}
+
+// A data structure to store all the info about 
+// the 'story' of the game. All the dungeon levels, etc
+class Campaign
+{
+  public Town? Town { get; set; }
+  public FactDb? FactDb { get; set; }
+  public Dictionary<int, Dungeon> Dungeons = [];
+  public List<MonsterDeck> MonsterDecks { get; set; } = [];
+
+  public void AddDungeon(Dungeon dungeon)
+  {
+    int id = Dungeons.Count == 0 ? 0 : Dungeons.Keys.Max() + 1;
+    Dungeons.Add(id, dungeon);
+  }
+}
