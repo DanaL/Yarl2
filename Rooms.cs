ac11a04294e8acd2f29aadba9435f5885da92dd1
Dana Larose <ywg.dana@gmail.com>
2025-02-05 13:47:41 -0600

    Skip trying to make a chasm trigger room if we couldn't find any exits

--- Rooms.cs
+++ Rooms.cs
@@ -539,7 +539,7 @@ class Rooms
     Map mapBelow = levels[level + 1];
 
     ChasmRoomInfo info = ChasmRoomInfo(map, room);
-    if (info.ChasmSqs.Count == 0 || info.IslandSqs.Count == 0) // || info.Exits.Count == 0)
+    if (info.ChasmSqs.Count == 0 || info.IslandSqs.Count == 0 || info.Exits.Count == 0)
       return;
 
     MakeChasm(map, mapBelow, info.ChasmSqs);
