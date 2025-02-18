0b4c95d31844e903add7e5101963cdefa9805775
Dana Larose <ywg.dana@gmail.com>
2025-02-17 20:06:05 -0600

    FrighteningTrait

--- Traits.cs
+++ Traits.cs
@@ -1192,6 +1192,13 @@ class FrightenedTrait : TemporaryTrait
   }
 }
 
+class FrighteningTrait : Trait 
+{
+  public int DC { get; set; }
+  
+  public override string AsText() => $"Frightening#{SourceId}#{DC}";
+}
+
 class FullBellyTrait : Trait, IGameEventListener
 {
   public ulong VictimID { get; set; }
@@ -2738,6 +2745,7 @@ class TraitFactory
     { "Frightened", (pieces, gameObj) => new FrightenedTrait()
       { OwnerID = ulong.Parse(pieces[1]), DC = int.Parse(pieces[2]), ExpiresOn = ulong.Parse(pieces[3]) }
     },
+    { "Frightening", (pieces, gameObj) => new FrighteningTrait() { SourceId = ulong.Parse(pieces[1]), DC = int.Parse(pieces[2])} },
     { "FullBelly", (pieces, gameObj) => new FullBellyTrait()
       {
         VictimID = ulong.Parse(pieces[1]),
