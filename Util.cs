ec08e17534c2773831a05995922862c5998a3429
Dana Larose <ywg.dana@gmail.com>
2025-03-03 19:05:10 -0600

    Bumped version to 0.3.3

--- Util.cs
+++ Util.cs
@@ -156,7 +156,7 @@ class Constants
   public const char SEPARATOR = '\u001F';
   public static int PRACTICE_RATIO = 100; // how skill use count translates into a bonus
   public const int TELEPATHY_RANGE = 40; // I don't really have a better spot for this right now
-  public static readonly string VERSION = "0.3.2";
+  public static readonly string VERSION = "0.3.3";
   public const char TOP_LEFT_CORNER = '┍';
   public const char TOP_RIGHT_CORNER = '┑';
   public const char BOTTOM_LEFT_CORNER = '┕';
