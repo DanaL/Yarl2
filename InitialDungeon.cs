f871a8530a2770fefef9a77c09d996cf8d2099ee
Dana Larose <ywg.dana@gmail.com>
2025-09-23 16:49:44 -0500

    Added kobold bully monster

--- InitialDungeon.cs
+++ InitialDungeon.cs
@@ -9,8 +9,6 @@
 // with this software. If not, 
 // see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-using System.Text.RegularExpressions;
-
 namespace Yarl2;
 
 class InitialDungeonBuilder(int dungeonID, (int, int) entrance, string mainOccupant) : DungeonBuilder
