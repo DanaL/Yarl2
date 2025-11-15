382bb40b6375108394d8febeda5f48baf7b40e83
Dana Larose <ywg.dana@gmail.com>
2025-11-14 19:01:03 -0600

    Was missing cyclopedia entry for giant toad

--- GameSetup.cs
+++ GameSetup.cs
@@ -559,7 +559,6 @@ class CampaignCreator(UserInterface ui)
   public GameState? Create(Options options)
   {
     int seed = DateTime.UtcNow.GetHashCode();
-    seed = 882177706;
     Console.WriteLine($"Seed: {seed}");
 
     try
