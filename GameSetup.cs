b6bb52d67219dc4b284261900bc91edf2e050d8f
Dana Larose <ywg.dana@gmail.com>
2026-01-27 15:21:00 -0600

    Missing cyclopedia entry for giant spiders

--- GameSetup.cs
+++ GameSetup.cs
@@ -568,7 +568,6 @@ class CampaignCreator(UserInterface ui)
   public GameState? Create(Options options)
   {
     int seed = DateTime.UtcNow.GetHashCode();
-    seed = -908442851;
     Console.WriteLine($"Seed: {seed}");
 
     try
