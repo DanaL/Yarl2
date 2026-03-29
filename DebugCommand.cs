77a94604658f41331f55b05d1ef59af81ac87c7a
Dana Larose <ywg.dana@gmail.com>
2026-03-29 10:38:10 -0500

    Halfling cupcakes -- an item that boosts your dexterity

--- DebugCommand.cs
+++ DebugCommand.cs
@@ -141,7 +141,8 @@ class DebugCommand(GameState gs)
     {"gloves of archery", ItemNames.GLOVES_OF_ARCHERY },
     {"shortbow", ItemNames.SHORTBOW },
     {"spider arrow", ItemNames.SPIDER_ARROW },
-    {"silver arrow", ItemNames.SILVER_ARROW }
+    {"silver arrow", ItemNames.SILVER_ARROW },
+    {"halfling cupcake", ItemNames.HALFLING_CUPCAKE }
   };
 
   public string DoCommand(string txt)
