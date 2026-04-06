5025dccef6f96a46f40fa67154face8c71fd2470
Dana Larose <ywg.dana@gmail.com>
2026-04-06 15:59:17 -0500

    Don't equip pick axe as part of the dig action

--- UseItemAction.cs
+++ UseItemAction.cs
@@ -19,20 +19,6 @@ class DigAction(GameState gs, Actor actor, Item tool) : Action(gs, actor)
 
   public override double Execute()
   {
-    if (!Tool.Equipped)
-    {
-      var (equipResult, _) = ((Player)Actor!).Inventory.ToggleEquipStatus(Tool.Slot);
-      if (equipResult != EquipingResult.Equipped)
-      {
-        GameState!.UIRef().SetPopup(new Popup($"You are unable to ready {Tool.Name.DefArticle()}!", "", -1, -1));
-        return 0.0;
-      }
-      else
-      {
-        GameState!.UIRef().AlertPlayer($"You equip {Tool.Name.DefArticle()}.");
-      }
-    }
-
     // if the actor is currently swallowed, trying to dig should attack
     // the monster instead
     if (Actor!.Traits.OfType<SwallowedTrait>().FirstOrDefault() is SwallowedTrait swallowed)
@@ -418,21 +404,6 @@ class ChopWoodAction(GameState gs, Actor actor, Item tool) : Action(gs, actor)
 
   public override double Execute()
   {
-    // Experimenting with not required an axe to be equipped when chopping
-    // if (!Tool.Equipped)
-    // {
-    //   var (equipResult, _) = ((Player)Actor!).Inventory.ToggleEquipStatus(Tool.Slot);
-    //   if (equipResult != EquipingResult.Equipped)
-    //   {
-    //     GameState!.UIRef().SetPopup(new Popup($"You are unable to ready {Tool.Name.DefArticle()}!", "", -1, -1));
-    //     return 0.0;
-    //   }
-    //   else
-    //   {
-    //     GameState!.UIRef().AlertPlayer($"You equip {Tool.Name.DefArticle()}.");
-    //   }
-    // }
-
     if (Actor!.Traits.OfType<SwallowedTrait>().FirstOrDefault() is SwallowedTrait swallowed)
     {
       if (GameState!.ObjDb.GetObj(swallowed.SwallowerID) is Actor target)
