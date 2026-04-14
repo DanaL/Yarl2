0209dcf6e014758b1581861e571d21a51d4b4329
Dana Larose <ywg.dana@gmail.com>
2026-04-14 16:19:50 -0500

    Sometimes add leather boots in collapsed tunnels

--- DungeonFeatures.cs
+++ DungeonFeatures.cs
@@ -780,6 +780,11 @@ class TunnelCarver
       objDb.SetToLoc(loc, ItemFactory.Get(ItemNames.WOOL_CLOAK, objDb));
     }
     
+    if (rng.Next(5) == 0)
+    {
+      objDb.SetToLoc(loc, ItemFactory.Get(ItemNames.LEATHER_BOOTS, objDb));
+    }
+
     // Maybe a skull (or eventually other remains?)
     if (rng.Next(3) == 0)
     {
